4714fe440484018e3a6439bc21302f0ce32d1518
dodo-_- <anigen.off@gmail.com>
2025-05-29 23:41:12 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ switch (env) {
     case 'local':
         config.port = 64395;
         config.local = "http://127.0.0.1:" + config.port;
-        console.log(`Mode local activé. URL locale: ${config.local}, Port: ${config.port}`);
+        console.log(`Mode local activé. URL de configuration: ${config.local}, Port: ${config.port}`);
         break;
     case 'production':
         config.port = process.env.PORT || 3951;
         config.local = process.env.HOST_NAME;
-        console.log(`Mode production activé. URL locale: ${config.local}, Port: ${config.port}`);
+        console.log(`Mode production activé. URL de configuration: ${config.local}, Port: ${config.port}`);
         break;
 }
 
