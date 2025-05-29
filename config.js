4d8e1e6a467af24bbfca9a56d5c3d5ebb32af1a8
dodo-_- <anigen.off@gmail.com>
2025-05-29 19:05:35 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var env = 'local';
+var env = process.env.NODE_ENV || 'local';
 
 var config = {
     BaseURL: "https://api.gestdown.info",
@@ -7,9 +7,16 @@ var config = {
 
 switch (env) {
     case 'local':
-		config.port = 64395
+        config.port = 64395;
         config.local = "http://127.0.0.1:" + config.port;
         break;
+    case 'production':
+        config.port = process.env.PORT || 3000;
+        config.local = process.env.APP_URL || `http://0.0.0.0:${config.port}`;
+        break;
+    default:
+        config.port = 64395;
+        config.local = "http://127.0.0.1:" + config.port;
 }
 
 module.exports = config;
