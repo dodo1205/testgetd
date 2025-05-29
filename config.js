bc0367581aebfc5ca62b013e1802d8950108fd09
dodo-_- <anigen.off@gmail.com>
2025-05-29 19:25:43 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,14 +9,13 @@ switch (env) {
     case 'local':
         config.port = 64395;
         config.local = "http://127.0.0.1:" + config.port;
+        console.log(`Mode local activé. URL locale: ${config.local}, Port: ${config.port}`);
         break;
     case 'production':
-        config.port = process.env.PORT || 3000;
-        config.local = process.env.APP_URL || `http://0.0.0.0:${config.port}`;
+        config.port = process.env.PORT || 3951;
+        config.local = process.env.HOST_NAME;
+        console.log(`Mode production activé. URL locale: ${config.local}, Port: ${config.port}`);
         break;
-    default:
-        config.port = 64395;
-        config.local = "http://127.0.0.1:" + config.port;
 }
 
 module.exports = config;
